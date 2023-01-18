1e525ffab94bd89b90f6d402594b04ec4c4deecc
E.R. Manaois <erthewise@gmail.com>
2023-01-18 18:07:59 +0800

    added redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
-import { compose, applyMiddleware } from 'redux';
+import { compose, applyMiddleware, combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/userSlice';
 import categoriesReducer from './categories/categoriesSlice';
@@ -9,11 +11,23 @@ const middleWares = [];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  categories: categoriesReducer,
+  cart: cartReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    categories: categoriesReducer,
-    cart: cartReducer
-  },
+  reducer: persistedReducer,
   enhancers: composedEnhancers
 })
+
+export const persistor = persistStore(store);
